bc01faa74c723078959322edea7fa8e111e71ad3
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-05-19 02:05:50 +0100

    Data added

--- app/src/main/java/com/example/transaction/MainActivity.kt
+++ app/src/main/java/com/example/transaction/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.transaction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -20,8 +25,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.transaction.ui.theme.TransactionTheme
 import com.example.transaction.ui.theme.backgroundcolor
+import com.example.transaction.ui.theme.labelColor
+import com.example.transaction.ui.theme.textValue
 import com.example.transaction.ui.theme.topBarColor
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +49,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TransactionScreen(modifier: Modifier = Modifier) {
+fun TransactionScreen(
+    modifier: Modifier = Modifier,
+    
+) {
     Box(modifier = Modifier.fillMaxSize()){
         AppBar()
     }
@@ -70,6 +81,86 @@ fun AppBar(){
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundcolor),
     )
 }
+
+@Composable
+fun ScreenItem(label: String, value: String, modifier: Modifier = Modifier){
+    val labelColor = labelColor
+    val textColor = textValue
+
+    Column(modifier = Modifier) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.titleSmall,
+            color = labelColor,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = value,
+            style = MaterialTheme.typography.bodySmall,
+            color = textColor
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
+}
+
+@Composable
+fun MeterDetails(
+    @StringRes meterNo: Int,
+    @StringRes meterName: Int,
+    @StringRes phoneNo: Int,
+    numLabel: Int,
+    nameLabel: Int,
+    phoneLabel: Int,
+    modifier: Modifier = Modifier
+){
+
+    Column(modifier = Modifier) {
+        ScreenItem(label = stringResource(numLabel), value = stringResource(meterNo))
+        ScreenItem(label = stringResource(nameLabel), value = stringResource(meterName))
+        ScreenItem(label = stringResource(phoneLabel), value = stringResource(phoneNo))
+    }
+
+}
+
+@Composable
+fun ServiceDetails(
+    @StringRes serviceProv: Int,
+    @StringRes product: Int,
+    @StringRes address: Int,
+    @StringRes timestamp: Int,
+    serviceProvLabel: Int,
+    productLabel: Int,
+    addressLabel: Int,
+    timeLabel: Int,
+){
+    Column(modifier = Modifier) {
+        ScreenItem(label = stringResource(serviceProvLabel), value = stringResource(serviceProv))
+        ScreenItem(label = stringResource(productLabel), value = stringResource(product))
+        ScreenItem(label = stringResource(addressLabel), value = stringResource(address))
+        ScreenItem(label = stringResource(timeLabel), value = stringResource(timestamp))
+    }
+
+}
+
+@Composable
+fun PaymentDetails(
+    @StringRes amount: Int,
+    @StringRes numOfUnit: Int,
+    @StringRes servCharge: Int,
+    amountLabel: Int,
+    numOfUnitLabel: Int,
+    servChargeLabel: Int
+){
+    Column(modifier = Modifier) {
+        ScreenItem(label = stringResource(amountLabel), value = stringResource(amount))
+        ScreenItem(label = stringResource(numOfUnitLabel), value = stringResource(numOfUnit))
+        ScreenItem(label = stringResource(servChargeLabel), value = stringResource(servCharge))
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
